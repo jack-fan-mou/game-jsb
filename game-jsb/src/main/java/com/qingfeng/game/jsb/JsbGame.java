225ffde83a3e07d67ed3bd80395a1e672197753a
jack-fan-mou <13917836009@163.com>
2017-09-26 13:45:46 +0800

    add first game

---
+++ game-jsb/src/main/java/com/qingfeng/game/jsb/JsbGame.java
@@ -0,0 +1,18 @@
+package com.qingfeng.game.jsb;
+
+import java.util.Date;
+
+public class JsbGame {
+
+    public  static  String hi(String word){
+        return  "Hi:" + word;
+    }
+
+    public String print(String info){
+        return  info;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Date().toString());
+    }
+}
